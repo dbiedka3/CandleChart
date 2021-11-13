176d38ba688751d888ba76fccbef77e4660a29c1
danbie <danbie@st.amu.edu.pl>
2021-11-13 19:53:25 +0100

    Initial commit

---
+++ src/main/java/shared/Price.java
@@ -0,0 +1,14 @@
+package shared;
+
+import lombok.Getter;
+
+@Getter
+public class Price {
+
+    private String label;
+    private double open;
+    private double high;
+    private double low;
+    private double close;
+
+}
